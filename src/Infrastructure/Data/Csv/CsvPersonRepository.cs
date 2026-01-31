8c3cbe1120d39be4e905686fe6a812502d1acbe8
romainjbr <romainjbrafai@gmail.com>
2026-01-31 13:42:31 +0100

    Implement CvsPersonRepository get operations

--- src/Infrastructure/Data/Csv/CsvPersonRepository.cs
+++ src/Infrastructure/Data/Csv/CsvPersonRepository.cs
@@ -7,23 +7,44 @@ public class CsvPersonRepository : IPersonRepository
 {
     private readonly PersonCsvReader _reader; 
 
+    private List<Person>? _people;
+
+    private List<Person> People
+    {
+        get
+        {
+            if (_people != null) 
+            { 
+                return _people;
+            }
+
+            _people = _reader.ReadCsv();   
+             
+            return _people;        
+        }
+    }
+
     public CsvPersonRepository(PersonCsvReader reader)
     {
         _reader = reader;
     }
-    
+
     public Task<List<Person>> GetAllPeopleAsync(CancellationToken token)
     {
-        throw new NotImplementedException();
+        return Task.FromResult(People);
     }
 
     public Task<List<Person>> GetPeopleByColorAsync(int colourNum, CancellationToken token)
     {
-        throw new NotImplementedException();
+        var peopleByColour = People.Where(p => p.Color == colourNum).OrderBy(p => p.Id).ToList();
+
+        return Task.FromResult(peopleByColour);
     }
 
     public Task<Person?> GetPersonByIdAsync(int id, CancellationToken token)
     {
-        throw new NotImplementedException();
+        var person = People.FirstOrDefault(p => p.Id == id);
+
+        return Task.FromResult(person);
     }
 }
