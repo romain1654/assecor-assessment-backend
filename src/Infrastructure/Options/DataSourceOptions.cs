aeca9c94d1417469c4c834cbcec40cb718f42be9
romainjbr <romainjbrafai@gmail.com>
2026-01-31 15:12:03 +0100

    Add DataSourceOptions and CsvOptions to carry CSV datasource information

---
+++ src/Infrastructure/Options/DataSourceOptions.cs
@@ -0,0 +1,7 @@
+namespace Infrastructure.Options;
+
+public sealed class DataSourceOptions
+{
+    public string Provider { get; init; } = "Csv";
+    public CsvOptions Csv { get; init; } = new();
+}
